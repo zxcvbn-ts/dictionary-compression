818dd9224372705e809f84eede5d447e6b3ee875
MrWook <mr.wook@web.de>
2024-10-04 09:57:03 +0200

    feat(project): remove index file
    BREAKING CHANGE: we don't need this index file

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,16 @@
-export default {
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
   entry: ['src/**/*.ts'],
   format: ["esm", "cjs"],
   dts: true,
   bundle: false,
   sourcemap: true,
+  clean: true,
   outExtension({ format }) {
     const extension = format === 'esm' ? '.mjs' : '.cjs'
     return {
       js: `${extension}`,
     }
   },
-};
+})
